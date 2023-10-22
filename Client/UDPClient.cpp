7f3daaab551126a3109c8b36b0c14cca220de5b5
kakaoleaves <kakaoleaves@naver.com>
2023-10-22 22:22:49 +0900

    UDP

---
+++ Client/UDPClient.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib") // Winsock Library
+
+using namespace std;
+
+int main(int argc, char* argv[]) // three arguments to be checked later
+{
+	string servName;	// server name
+	int servPort;		// server port number
+	char sendBuffer[1000];
+
+	// Check and set arguments
+	if (argc != 3) // 프로그램 이름도 포함됨
+	{
+		cout << "Error : three arguments are needed!" << endl;
+		exit(1);
+	}
+
+	servName = argv[1];
+	servPort = atoi(argv[2]);
+
+	// Initialize Winsock
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		return 1;
+	}
+
+	// Create Socket
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int errCode = ::WSAGetLastError();
+		cout << "socket() failed with error code : " << errCode << endl;
+		return 1;
+	}
+
+	// Cratee remote (server) socket address
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, servName.c_str(), &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons((u_short)servPort);
+
+	// Send data to the server
+	while (true)
+	{
+		cout << "Input message to send(q to quit) : ";
+		cin.getline(sendBuffer, 1000);
+
+		if (strcmp(sendBuffer, "q") == 0)
+			break;
+
+		int sendBytes = ::sendto(clientSocket, sendBuffer, (int)strlen(sendBuffer), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+		if (sendBytes == SOCKET_ERROR)
+		{
+			int errCode = ::WSAGetLastError();
+			cout << "send() failed with error code : " << errCode << endl;
+			return 1;
+		}
+
+		// Receive data from the server
+		char recvBuffer[1000];
+
+		SOCKADDR_IN recvAddr;
+		::memset(&recvAddr, 0, sizeof(recvAddr));
+		int recvAddrSize = sizeof(recvAddr);
+
+		int recvBytes = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0, (SOCKADDR*)&recvAddr, &recvAddrSize);
+
+		if (recvBytes == SOCKET_ERROR)
+		{
+			int errCode = ::WSAGetLastError();
+			cout << "recv() failed with error code : " << errCode << endl;
+			return 1;
+		}
+		else if (recvBytes == 0)
+		{
+			cout << "Connection closed by peer" << endl;
+			break;
+		}
+		else
+		{
+			recvBuffer[recvBytes] = '\0';
+			cout << "Received message : " << recvBuffer << endl;
+		}
+	}
+
+
+
+	// Close the socket
+	::closesocket(clientSocket);
+
+	// Finalize Winsock
+	::WSACleanup();
+
+	return 0;
+}
